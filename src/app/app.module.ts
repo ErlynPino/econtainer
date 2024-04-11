652486b89f6a5471d41a33a5b36e201acffb61ab
ErlynPinoTorres <98849964+ErlynPinoTorres@users.noreply.github.com>
2024-04-11 14:32:38 -0500

    proyecto Econtainer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BuscarComponent,
+    HomeComponent,
+    HeroeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
